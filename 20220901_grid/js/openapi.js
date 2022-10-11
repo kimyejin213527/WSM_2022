10b377e8e65864c92ee2de202bd3028619814713
kimyejin213527 <kimyejin213527@gmail.com>
2022-10-11 16:03:52 +0900

    api에서 급식 url 불러오기

--- 20220901_grid/js/openapi.js
+++ 20220901_grid/js/openapi.js
@@ -1,20 +1,29 @@
-// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN17320190722180924242823&infSeq=2
-// ? 뒤에는 추가 정보
-// 시도교육청코드: B10: 서울특별시교육청
-// 표준학교코드: 7010569: 미림여자정보과학고등학교
-// 식사코드: 2: 중식
+//시도교육청 코드 B10: 서울특별시교육청
+// 표준학교 코드 7010569
+//식사코드: 2: 중식
 
-// 신청주소: https://open.neis.go.kr/hub/mealServiceDietInfo // 요청주소
-// KEY:
-// ATPT_OFCDC_CODE: 시도교육청코드
-// SD_SCHU_SC_CODE: 표준학교코드
-// MMEAL_SC_CODE: 식사코드
-// MLSV_YMD: 급식일자
-// MLSV_FROM_YMD: 급식시작일자
-// MLSV_TO_YMD: 급식종료일자
-// https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_YMD=0220927&Type=json
-const KEY = "f16e34988ef7450c9505c89b7d1260a2";
-const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시교육청
-const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
-let MMEAL_SC_CODE = 2;  // 중식
-let MLSV_YMD = "20220927";  // YYYYMMDD
+//KEY:
+// ATPT_OFCDC_SC_CODE	     STRING(필수)	    시도교육청코드
+//     SD_SCHUL_CODE	     STRING(필수)	    표준학교코드
+//     MMEAL_SC_CODE	     STRING(선택)	    식사코드
+//     MLSV_YMD	     STRING(선택)	    급식일자
+//     MLSV_FROM_YMD	     STRING(선택)	    급식시작일자
+//     MLSV_TO_YMD	     STRING(선택)	    급식종료일자
+
+//  https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_YMD=20220927&Type=json
+
+
+
+const KEY = "f9f06af16f3449b6860a8b365af1f232";
+const ATPT_OFCDC_SC_CODE = "B10";
+const c = "7010569";
+let MMEAL_SC_CODE = 2;
+let MLSV_YMD = "20220927";
+
+let url = `https://open.neis.go.kr/hub/mealServiceDietInfo?`
+    + `KEYE=${KEY}`
+    + `ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`
+    + `SD_SCHUL_CODE=${SD_SCHUL_CODE}`
+    + `MMEAL_SC_CODE=${MMEAL_SC_CODE}`
+    + `MLSV_YMD=${MLSV_YMD}`;
+console.log(url);
